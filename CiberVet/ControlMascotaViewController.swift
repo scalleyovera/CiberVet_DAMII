d1c47eba5d12cd60b280387433189dd90c00a36e
grupo_dam_cibertec <scalleyovera@gmail.com>
2025-04-28 03:08:29 -0400

    [FEAT] add full calendar, health control, social view

---
+++ CiberVet/ControlMascotaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ControlMascotaViewController.swift
+//  CiberVet
+//
+//  Created by user279229 on 4/28/25.
+//
+
+import UIKit
+
+class ControlMascotaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
