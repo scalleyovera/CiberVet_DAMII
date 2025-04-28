98e69489d228d8b290c42389583246f798295033
grupo_dam_cibertec <scalleyovera@gmail.com>
2025-04-27 22:15:52 -0400

    [FIX] Arregle el table view, modifque el disenio pero esta pendiente mejorar constraints

--- CiberVet/HomeViewController.swift
+++ CiberVet/HomeViewController.swift
@@ -19,9 +19,9 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tablaMascotas.dataSource = self
-        tablaMascotas.delegate = self
-        cargarMascotas()
+        //tablaMascotas.dataSource = self
+        //tablaMascotas.delegate = self
+        //cargarMascotas()
 
         // Do any additional setup after loading the view.
     }
@@ -80,29 +80,28 @@ class HomeViewController: UIViewController {
         
     }
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension HomeViewController: UITableViewDataSource {
-    func tableView(_ 16: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return mascotas.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "celda mascota", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "celdaMascota", for: indexPath) as! MascotaTableViewCell
         let mascota = mascotas[indexPath.row]
         
         return cell
+    }
+}
+
+extension HomeViewController : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            self.context.delete(mascotas[indexPath.row])
+            mascotas.remove(at: indexPath.row)
+            
+            tableView.reloadData()
+        }
     }
 }
