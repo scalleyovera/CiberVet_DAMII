aa78b3ec7e1631b2227be934841f201db0975ee7
grupo_dam_cibertec <scalleyovera@gmail.com>
2025-05-03 02:34:42 -0400

    [FEAT All interfaces ended

---
+++ CiberVet/RegisterPetViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RegisterPetViewController.swift
+//  CiberVet
+//
+//  Created by user279229 on 5/3/25.
+//
+
+import UIKit
+
+class RegisterPetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func returnTapped(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
