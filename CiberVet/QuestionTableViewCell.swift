94d4c96baf63f01623097a572fd9ee723c58d4c0
grupo_dam_cibertec <scalleyovera@gmail.com>
2025-05-04 04:53:04 -0400

    [FEAT] Call API completed

---
+++ CiberVet/QuestionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  QuestionTableViewCell.swift
+//  CiberVet
+//
+//  Created by user279229 on 5/4/25.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
